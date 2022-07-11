262d73b4cd639d807d9d82d821b28915833c40c3
Luiss Resources <luissresources@gmail.com>
2022-07-11 00:37:32 -0500

    Updates

---
+++ precache-manifest.587f334c6512c651233fc25bec9f3093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17e087a390eda4569a0a",
+    "url": "/memes-generator/static/css/main.edc03f6b.chunk.css"
+  },
+  {
+    "revision": "17e087a390eda4569a0a",
+    "url": "/memes-generator/static/js/main.17e087a3.chunk.js"
+  },
+  {
+    "revision": "4c5a667f7676a9678459",
+    "url": "/memes-generator/static/js/1.4c5a667f.chunk.js"
+  },
+  {
+    "revision": "9a06c32ae9df79547a80",
+    "url": "/memes-generator/static/js/runtime~main.9a06c32a.js"
+  },
+  {
+    "revision": "f48e74e9e15d6682f4684eaa4ced1092",
+    "url": "/memes-generator/static/media/logo-2.f48e74e9.png"
+  },
+  {
+    "revision": "8ca912fe875996119fe702593a2b999b",
+    "url": "/memes-generator/index.html"
+  }
+];
